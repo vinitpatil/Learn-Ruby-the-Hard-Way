a159084c8c1a3fc130d3590de08684108ebd22f5
Vinit Patil <vinit@thepricerie.com>
2012-01-24 07:31:01 -0800

    Changed old to weird
    Coz weird is the new old.

--- ex12.rb
+++ ex12.rb
@@ -1,8 +1,8 @@
-print "How old are you?"
+print "How weird are you?"
 age = gets.chomp()
 print "How tall are you?"
 height = gets.chomp()
 print "How much do you weigh?"
 weight = gets.chomp()
 
-puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+puts "So, you're #{age} weird, #{height} tall and #{weight} heavy."
