4090a77a693487bfbacc08d4fbf7de3e89c6496f
Vinit Patil <vinit@thepricerie.com>
2012-01-30 11:20:13 -0800

    updated scripts

--- ex14.rb
+++ ex14.rb
@@ -1,21 +1,23 @@
-user = ARGV.first
-prompt = 'speak! ' #changed prompt = > to speak for fun
+# Here, we are trying to get input from the user via ARGV and gets chomp in the same script. First we get input from the user through an ARGV command. Then we get two inputs from the user through gets.chomp command. With ARGV user will enter the value at the terminal prompt.
 
-puts "Hi #{user}, I'm the #{$O} script." 
-puts "I'd like to ask you a few questions."
-puts "Do you like me #{user}?"
+user = ARGV.first #Adding first to ARGV picks up the first value assigned to the variable.Simple.
+prompt = '>' #This is really a convenience thing. Assigning the > to prompt makes it easier for the me to display the '>' after a question instead of going through the slightly more tedious process of putting > inside quotations over and over after every question. I mean is it easier to type print prompt or print '>'. C'mon you know you hate syntax. 
+
+puts "Hi #{user}. I'm the #{$0} script"
+puts "I would like to ask you a few questions"
+puts "Do you like me #{user}. Answer yes or no?"
 print prompt
-likes = STDIN.gets.chomp()
+like = STDIN.gets.chomp() #i'm asking the user to say whether he likes me or not
 
 puts "Where do you live #{user}?"
-print prompt 
-lives = STDIN.gets.chomp()
-
-puts "What kind of computer do you have?"
 print prompt
-computer = STDIN.gets.chomp()
+live = STDIN.gets.chomp()
+
+puts "While painting, do you like to use the spray or brush #{user}?"
+print prompt
+paint = STDIN.gets.chomp()
 
 puts <<MESSAGE
-Alright so you said you #{likes} about liking me. You live in #{lives}. Not sure where that is. And you have a #{computer}. Nice."
+So about liking me, I'm surprised you said #{like}. And you live in the nice city of #{live}. And your favorite style of painting is #{paint} painting.  
 MESSAGE
-
+ 
--- ex15.rb
+++ ex15.rb
@@ -1,15 +1,21 @@
-filename = ARGV.first
-prompt = "> "
+# In this exercise we open a file that the user needs to see and display the contents. The user inputs the filename through ARGV command. Then we try it with STDIN.gets. The command for opening the file is File.open and reading is File.read
 
-txt = File.open(filename)
+#Steps 1 Let the user assign the name of the file to the variable filename
+#commands open File.open(name of file)
+#read filename.read()
 
-puts "Here's your file #{filename}"
-puts txt.read()
+filename = ARGV.first #get the filename from the user
 
-puts "What is the name of your file again?"
-print prompt
-filename_again = STDIN.gets.chomp()
+puts "the file you want me to read is #{filename}"  #unnecessary. tell the user what filename we are going to open and read.
+txt = File.open(filename) #open the file and assign it to variable txt
+puts txt.read() #read the contents of the file and print it with puts command. 
 
-txt_again = File.open(filename_again)
+puts "What was that file again?" #try the same process with STDIN.gets 
+print '>'
+file_again = STDIN.gets.chomp() #ask the user for the filename
+txt_again = File.open(file_again) #open it and assign the contents to variable txt
+puts txt_again.read() #read the contents and print it with puts commands
 
-puts txt_again.read()
+
+
+
